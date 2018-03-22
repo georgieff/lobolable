9a2c6d7641888d88774661a5f8c9121da1b4a5d3
georgieff <lubo@georgieff.it>
2018-03-22 20:42:22 +0200

    fix label in item view

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,9 +1,11 @@
 from django.forms import ModelForm
+from django import forms
 
 from .models import PizzaComment
 
 
 class PizzaCommentForm(ModelForm):
+    comment = forms.CharField(widget=forms.Textarea, label='Your comment')
     class Meta:
         model = PizzaComment
         exclude = ('user', 'pizza', 'date_added')
